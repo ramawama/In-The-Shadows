9cd9f488f5e6ffd8393ea0c6dd0238c181f3d5da
csobolew <47485699+csobolew@users.noreply.github.com>
2023-06-11 23:51:46 -0400

    New window and text :wrench:

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+import pygame
+
+
 def main():
-    print('Hello World!')
-    print('Deven Ellis')
-    print('Ryan Lewis')
-    print('keanu chung')
-    print('senor\n')
+    white = (255, 255, 255)
+    black = (0, 0, 0)
+    red = (255, 0, 0)
+    (width, height) = (896, 504)
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.flip()
+    pygame.font.init()
+    font = pygame.font.get_default_font()
+    font = pygame.font.Font(font, 32)
+    text = font.render('IN THE SHADOWS', True, black)
+    textRect = text.get_rect()
+    textRect.center = (width // 2, height // 2)
+
+
+    while True:
+        screen.fill(white)
+        square2 = pygame.transform.scale(pygame.Surface((16, 16)), (64, 64))
+        square2.fill(red)
+        screen.blit(square2, (50, 50))
+        screen.blit(text, textRect)
+        pygame.display.update()
+        pass
 
 
 if __name__ == "__main__":
