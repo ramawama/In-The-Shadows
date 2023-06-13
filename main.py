e36c715f930b1c19a374779468c726434b9a46f2
ramawama <93228765+ramawama@users.noreply.github.com>
2023-06-13 09:39:09 +0900

    Created start button, tracking mouse pos now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import sys
 
 def main():
     white = (255, 255, 255)
@@ -12,17 +12,30 @@ def main():
     pygame.font.init()
     font = pygame.font.get_default_font()
     font = pygame.font.Font(font, 32)
-    text = font.render('IN THE SHADOWS', True, black)
+    text = font.render('IN THE SHADOWS', True, white)
     textRect = text.get_rect()
-    textRect.center = (width // 2, height // 2)
+    textRect.center = (width // 2, 32)
+    start_text = font.render('START', True, white)
+    start_text_rect = start_text.get_rect()
+    start_text_rect.center = (width // 2, height // 2)
 
 
     while True:
-        screen.fill(white)
+        for ev in pygame.event.get():
+            if ev.type == pygame.MOUSEBUTTONDOWN:
+
+                # if the mouse is clicked on the
+                # button the game is terminated
+                if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                    pygame.quit()
+
+        screen.fill(black)
+        mouse = pygame.mouse.get_pos()
         square2 = pygame.transform.scale(pygame.Surface((16, 16)), (64, 64))
         square2.fill(red)
         screen.blit(square2, (50, 50))
         screen.blit(text, textRect)
+        screen.blit(start_text, start_text_rect)
         pygame.display.update()
         pass
 
