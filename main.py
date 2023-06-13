3bc7b032432c3877ddfbec3bf106f0f8a64b7afd
ramawama <93228765+ramawama@users.noreply.github.com>
2023-06-13 10:02:18 +0900

    added quit button, closes using when mousbutton pressed and in bounds

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
-import sys
+
 
 def main():
     white = (255, 255, 255)
     black = (0, 0, 0)
     red = (255, 0, 0)
     (width, height) = (896, 504)
+    (start_width, start_height) = (width // 2, height // 2)
+    (quit_width, quit_height) = (start_width , start_height + 64)
 
     screen = pygame.display.set_mode((width, height))
     pygame.display.flip()
@@ -17,16 +19,19 @@ def main():
     textRect.center = (width // 2, 32)
     start_text = font.render('START', True, white)
     start_text_rect = start_text.get_rect()
-    start_text_rect.center = (width // 2, height // 2)
+    start_text_rect.center = (start_width, start_height)
+    quit_text = font.render('QUIT', True, white)
+    quit_rect = pygame.Rect(quit_width, quit_height, 64, 32)
+    quit_rect.center = (quit_width - 8, quit_height)
 
 
     while True:
         for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+                pygame.quit()
             if ev.type == pygame.MOUSEBUTTONDOWN:
-
-                # if the mouse is clicked on the
-                # button the game is terminated
-                if width / 2 <= mouse[0] <= width / 2 + 140 and height / 2 <= mouse[1] <= height / 2 + 40:
+                # if left-clicked
+                if quit_width - 64 <= mouse[0] <= quit_width and quit_height <= mouse[1] <= quit_height + 32:
                     pygame.quit()
 
         screen.fill(black)
@@ -36,8 +41,9 @@ def main():
         screen.blit(square2, (50, 50))
         screen.blit(text, textRect)
         screen.blit(start_text, start_text_rect)
+        screen.blit(quit_text, quit_rect)
         pygame.display.update()
-        pass
+
 
 
 if __name__ == "__main__":
