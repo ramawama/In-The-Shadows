0976b791358779f3abe8a6787c36e1f8e26e39ce
csobolew <47485699+csobolew@users.noreply.github.com>
2023-06-15 19:40:54 +0900

    Added old classes back for documentation/reference

---
+++ old_entities/easy_guard.py
@@ -0,0 +1,23 @@
+import pygame
+from enemy import Enemy
+class Easy_Guard(Enemy):
+    def __init__(self, direction="Down"):
+        super().__init__(self, direction)
+        self._state = "Alive"  # alive or dead
+        self._direction = direction  # the direction they are facing
+        self._left = pygame.image.load("./assets/graphics/Guard_easy.png")
+        self._right = pygame.image.load("./assets/graphics/Guard_easy.png")
+        self._up = pygame.image.load("./assets/graphics/Guard_easy.png")
+        self._down = pygame.image.load("./assets/graphics/Guard_easy.png")
+
+    @property
+    def image(self):
+        match self._direction:
+            case "left":
+                return self._left
+            case "right":
+                return self._right
+            case "up":
+                return self._up
+            case "down":
+                return self._down
