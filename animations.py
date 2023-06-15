0da63980be03660009135450bda8f7e9b2eb1160
Ryan17Lewis <ryanlewis@ufl.edu>
2023-06-15 13:48:49 +0900

    moveRight func working

--- animations.py
+++ animations.py
@@ -1,21 +1,30 @@
 import pygame
 
 
-# def moveRight(screen, x, y, distance, speed):
-#     guard = [pygame.image.load("assets/graphics/Rogue.png"),
-#              pygame.image.load("assets/graphics/Rogue_walk.png")]
-#     size = 32
-#     guard = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
-#     guard_right = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
-#
-#     guard_counter = 0
-#     while True:
-#         pygame.time.Clock().tick(speed)
-#         screen.blit(guard_right[guard_counter], (x, y))
-#         guard_counter += 1
-#         x += 8
+def moveRight(screen, x, y, rogue, background, speed):
+    distance = 32
+    anim_counter = 0
+    tile_x = x
+    tile_y = y
+    while True:
+        # draw background
+        screen.blit(background, (tile_x, tile_y))
+        if distance - 16 <= 0:
+            screen.blit(background, (tile_x + 32, tile_y))
+        pygame.time.Clock().tick(speed)
 
+        screen.blit(rogue[anim_counter], (x, y))
+        x += 4
+        distance -= 4
+        anim_counter += 1
 
+        if anim_counter >= len(rogue):
+            anim_counter = 0
+
+        if distance <= 0:
+            return x, y
+
+        pygame.display.update()
 
 
 def main():
@@ -30,52 +39,56 @@ def main():
     pygame.display.flip()
     pygame.font.init()
 
-    guard = [pygame.image.load("assets/graphics/Rogue/Rogue.png"),
-             pygame.image.load("assets/graphics/Rogue/Rogue_walk.png")]
-
+    # guard = [pygame.image.load("assets/graphics/Rogue/Rogue.png"),
+    #          pygame.image.load("assets/graphics/Rogue/Rogue_walk.png")]
+    #
     torch = [pygame.image.load("assets/graphics/Torch/Torch_small.png"),
              pygame.image.load("assets/graphics/Torch/Torch_big.png")]
-
-    size = 32
-    guard = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
-    guard_right = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
-    guard_left = [pygame.transform.flip(guard[0], True, False), pygame.transform.flip(guard[1], True, False)]
+    #
+    # size = 32
+    # guard = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
+    # guard_right = [pygame.transform.scale(guard[0], (size, size)), pygame.transform.scale(guard[1], (size, size))]
+    # guard_left = [pygame.transform.flip(guard[0], True, False), pygame.transform.flip(guard[1], True, False)]
     torch = [pygame.transform.scale(torch[0], (14, 20)), pygame.transform.scale(torch[1], (14, 20))]
-
-    clock = pygame.time.Clock()
-
-    guard_counter = 0
+    #
+    # clock = pygame.time.Clock()
+    #
+    # guard_counter = 0
     torch_counter = 0
-    location = 100
-    distance = 32*4
-    move_right = True
+    # location = 100
+    # distance = 32*4
+    # move_right = True
 
+    rogue = [pygame.image.load("assets/graphics/Rogue/Rogue.png"),
+             pygame.image.load("assets/graphics/Rogue/Rogue_walk.png")]
+    background = pygame.image.load("assets/graphics/o.png")
+    x, y = 20, 20
     quit_game = False
 
     while not quit_game:
-        clock.tick(4)
+        # clock.tick(4)
         screen.fill(white)
-
-        if guard_counter >= len(guard):
-            guard_counter = 0
 
         if torch_counter >= len(torch):
             torch_counter = 0
 
-        if move_right:
-            screen.blit(guard_right[guard_counter], (location, 100))
-            guard_counter += 1
-            location += 8
+        # if move_right:
+        #     screen.blit(guard_right[guard_counter], (location, 100))
+        #     guard_counter += 1
+        #     location += 8
+        #
+        #     if location - 100 >= distance:
+        #         move_right = False
+        # else:
+        #     screen.blit(guard_left[guard_counter], (location, 100))
+        #     guard_counter += 1
+        #     location -= 8
+        #
+        #     if location <= 100:
+        #         move_right = True
 
-            if location - 100 >= distance:
-                move_right = False
-        else:
-            screen.blit(guard_left[guard_counter], (location, 100))
-            guard_counter += 1
-            location -= 8
-
-            if location <= 100:
-                move_right = True
+        screen.blit(background, (x, y))
+        screen.blit(rogue[0], (x, y))
 
         screen.blit(torch[torch_counter], (200, 132))
         torch_counter += 1
@@ -85,6 +98,10 @@ def main():
             if ev.type == pygame.QUIT:
                 quit_game = True
                 pygame.quit()
+            if ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_RIGHT:
+                    print("press")
+                    x, y = moveRight(screen, x, y, rogue, background, 8)
 
 
 if __name__ == "__main__":
--- entities/player.py
+++ entities/player.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 class Player():
     def __init__(self, direction="Down"):
         self._state = "Alive"  # alive or dead
