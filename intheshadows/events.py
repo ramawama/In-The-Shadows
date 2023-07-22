b7c2f3575c6c90d1b3658fabe4088b260d56ff87
Deven Ellis <54079741+dellis7@users.noreply.github.com>
2023-07-22 14:40:55 +0900

    Revert "Move guards at same time"

--- intheshadows/events.py
+++ intheshadows/events.py
@@ -16,15 +16,32 @@ def game_over(width, height, screen, board):
     return level, state
 
 
-def win(width, height, screen, black, white):
+def win(width, height, screen, black, num_torches, num_items, turns_passed):
     level = 1
     state = "win"
     screen.background_surface.fill(black)
     screen.foreground_surface.fill(black)
-    big_font = pygame.font.Font(Path(__file__).parent / 'assets/fonts/Enchanted Land.otf', int(height * 0.2))
-    text = big_font.render('YOU  HAVE  WON', True, white)
-    text_rect = text.get_rect()
-    text_rect.center = (width // 2, height // 4)
-    screen.foreground_surface.blit(text, text_rect)
+
+    background = pygame.image.load(Path(__file__).parent / "assets/graphics/Backgrounds/Game_win_screen.png")
+    background = pygame.transform.scale(background, (width, height))
+    font = pygame.font.Font(Path(__file__).parent / 'assets/fonts/Minecraftia-Regular.ttf', int(height * 0.05))
+
+    torches_unlit = font.render(str(num_torches), True, (255, 255, 255))
+    torches_unlit_rect = torches_unlit.get_rect()
+    torches_unlit_rect.x, torches_unlit_rect.y = (7.7 * width // 12, 5.1 * height // 8)
+
+    items_used = font.render(str(num_items), True, (255, 255, 255))
+    items_used_rect = items_used.get_rect()
+    items_used_rect.x, items_used_rect.y = (7.7 * width // 12, 5.67 * height // 8)
+
+    num_turns = font.render(str(turns_passed), True, (255, 255, 255))
+    num_turns_rect = num_turns.get_rect()
+    num_turns_rect.x, num_turns_rect.y = (7.7 * width // 12, 6.23 * height // 8)
+
+    screen.foreground_surface.blit(background, (0, 0))
+    screen.foreground_surface.blit(torches_unlit, torches_unlit_rect)
+    screen.foreground_surface.blit(items_used, items_used_rect)
+    screen.foreground_surface.blit(num_turns, num_turns_rect)
+
     screen.update()
     return level, state
