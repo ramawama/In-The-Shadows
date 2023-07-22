2349ea038d307eeaf28c5a93cc05e129572b3d71
Deven <dellis1@ufl.edu>
2023-07-22 16:44:18 +0900

    items update on screen

--- intheshadows/game.py
+++ intheshadows/game.py
@@ -1083,10 +1083,6 @@ class Game:
                         print("Attempted to load a game asset but failed (this try/except is in run(self) method):", E)
                 case 'inventory':
                     display_info(self.__width, self.__height, self.__screen, self.__level, 0, 0, 0, self.__player.num_water, self.__player.num_smoke)
-                    '''
-                    TODO: Add some sort of data structure to store player inventory and pass it to display_inventory
-                    also have it track what items are used etc
-                    '''
                 case 'game_over':
                     pass
                 case 'move':
